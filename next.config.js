b71ddb5bde53465a13b640f7cc2a6396b0edb3d1
Umair Syed <itsumairsyed@gmail.com>
2023-04-06 16:25:22 +0500

    Initial commit: Tailwind and Store configuration completed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   experimental: {
     appDir: true,
   },
